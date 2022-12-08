26b11f87535d9d1f6d7ad594232179d3b20524c0
NabilEMAP <Nabil_EM@outlook.com>
2022-12-08 08:55:22 +0100

    Added Exception Map

---
+++ MovieCollection.BLL/Exceptions/EndDateBeforeCurrentDateException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCollection.BLL.Exceptions
+{
+    public class EndDateBeforeCurrentDateException : Exception
+    {
+        public EndDateBeforeCurrentDateException()
+        {
+
+        }
+
+        public EndDateBeforeCurrentDateException(DateTime endDate)
+            : base($"Enddate {endDate} lies before the current date.")
+        {
+
+        }
+    }
+}
