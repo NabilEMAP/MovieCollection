332a01b8bfe665bbc9bbd2d0d9aeff794ed6ae93
NabilEMAP <Nabil_EM@outlook.com>
2022-12-09 01:19:35 +0100

    Consume WebAPI in MVC GetAllGenres

--- MovieCollection.UI/Controllers/GenresController.cs
+++ MovieCollection.UI/Controllers/GenresController.cs
@@ -1,82 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MovieCollection.DAL.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using MovieCollection.UI.Models;
 using Newtonsoft.Json;
-using System;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Net.Http.Headers;
 
 namespace MovieCollection.API.Controllers
 {
     public class GenresController : Controller
     {
-        string baseURL = "https://localhost:8001/api/";
+        Uri baseAddress = new Uri("https://localhost:8001/api");
+        HttpClient client;
 
-        public async Task<IActionResult> Index()
+        public GenresController()
         {
-            IList<GenreViewModel> genre = new List<GenreViewModel>();
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
 
-            using (var client = new HttpClient())
+        public IActionResult Index()
+        {
+            List<GenreViewModel> modelList = new List<GenreViewModel>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/Genres").Result;
+            if (response.IsSuccessStatusCode)
             {
-                client.BaseAddress = new Uri(baseURL);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                
-                HttpResponseMessage getData = await client.GetAsync("Genres");
-
-                if (getData.IsSuccessStatusCode)
-                {
-                    string results = getData.Content.ReadAsStringAsync().Result;
-                    genre = JsonConvert.DeserializeObject<List<GenreViewModel>>(results);
-                }
-                else
-                {
-                    Console.WriteLine("Error calling WebAPI");
-                }
-                ViewData.Model = genre;
+                string data = response.Content.ReadAsStringAsync().Result;
+                modelList = JsonConvert.DeserializeObject<List<GenreViewModel>>(data);
             }
-            return View();
+            return View(modelList);
         }
 
-        public async Task<IActionResult> Create(GenreViewModel genre)
+        public IActionResult Create()
         {
-            GenreViewModel obj = new GenreViewModel()
-            {
-                Name = genre.Name,
-            };
 
-            if (genre.Name != null)
-            {
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri(baseURL + "Genres/");
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                    HttpResponseMessage getData = await client.PostAsJsonAsync<GenreViewModel>("addGenre", obj);
-
-                    if (getData.IsSuccessStatusCode)
-                    {
-                        return RedirectToAction("Index", "Genres");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error calling WebAPI");
-                    }
-                }
-            }
-            return View();
-        }
-
-        public async Task<IActionResult> Update()
-        {
-            return View();
-        }
-
-        public async Task<IActionResult> Delete()
-        {
             return View();
         }
     }
