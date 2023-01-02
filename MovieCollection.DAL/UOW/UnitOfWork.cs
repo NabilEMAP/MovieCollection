c8d23b14b69f8c0dfbbecb0723e8fb8f81d2010e
NabilEMAP <Nabil_EM@outlook.com>
2023-01-02 18:35:24 +0100

    UnitOfWork Updated

--- MovieCollection.DAL/UOW/UnitOfWork.cs
+++ MovieCollection.DAL/UOW/UnitOfWork.cs
@@ -11,7 +11,7 @@ namespace MovieCollection.DAL.UOW
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _context;
-        //private readonly ICountriesRepository _countriesRepository;
+        private readonly ICountriesRepository _countriesRepository;
         //private readonly IDirectorsRepository _directorsRepository;
         private readonly IGenresRepository _genresRepository;
         //private readonly IMoviesRepository _moviesRepository;
@@ -19,7 +19,7 @@ namespace MovieCollection.DAL.UOW
 
         public UnitOfWork(
             ApplicationDbContext context,
-            //ICountriesRepository countriesRepository,
+            ICountriesRepository countriesRepository,
             //IDirectorsRepository directorsRepository,
             //IMoviesRepository moviesRepository,
             //IUsersRepository usersRepository,
@@ -27,13 +27,13 @@ namespace MovieCollection.DAL.UOW
             )
         {
             _context = context;
-            //_countriesRepository = countriesRepository;
+            _countriesRepository = countriesRepository;
             //_directorsRepository = directorsRepository;
             //_moviesRepository = moviesRepository;
             //_usersRepository = usersRepository;
             _genresRepository = genresRepository;
         }
-        //public ICountriesRepository CountriesRepository { get { return _countriesRepository; } }
+        public ICountriesRepository CountriesRepository { get { return _countriesRepository; } }
         //public IDirectorsRepository DirectorsRepository { get { return _directorsRepository; } }
         //public IMoviesRepository MoviesRepository { get { return _moviesRepository; } }
         //public IUsersRepository UsersRepository { get { return _usersRepository; } }
