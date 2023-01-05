dedb9f224458d5f2fefdb61c2f8435ddb8fc4b48
NabilEMAP <Nabil_EM@outlook.com>
2023-01-05 13:39:50 +0100

    UnitOfWork Updated

--- MovieCollection.DAL/UOW/UnitOfWork.cs
+++ MovieCollection.DAL/UOW/UnitOfWork.cs
@@ -12,31 +12,31 @@ namespace MovieCollection.DAL.UOW
     {
         private readonly ApplicationDbContext _context;
         private readonly ICountriesRepository _countriesRepository;
-        //private readonly IDirectorsRepository _directorsRepository;
+        private readonly IDirectorsRepository _directorsRepository;
         private readonly IGenresRepository _genresRepository;
-        //private readonly IMoviesRepository _moviesRepository;
-        //private readonly IUsersRepository _usersRepository;
+        private readonly IMoviesRepository _moviesRepository;
+        private readonly IUsersRepository _usersRepository;
 
         public UnitOfWork(
             ApplicationDbContext context,
             ICountriesRepository countriesRepository,
-            //IDirectorsRepository directorsRepository,
-            //IMoviesRepository moviesRepository,
-            //IUsersRepository usersRepository,
+            IDirectorsRepository directorsRepository,
+            IMoviesRepository moviesRepository,
+            IUsersRepository usersRepository,
             IGenresRepository genresRepository
             )
         {
             _context = context;
             _countriesRepository = countriesRepository;
-            //_directorsRepository = directorsRepository;
-            //_moviesRepository = moviesRepository;
-            //_usersRepository = usersRepository;
+            _directorsRepository = directorsRepository;
+            _moviesRepository = moviesRepository;
+            _usersRepository = usersRepository;
             _genresRepository = genresRepository;
         }
         public ICountriesRepository CountriesRepository { get { return _countriesRepository; } }
-        //public IDirectorsRepository DirectorsRepository { get { return _directorsRepository; } }
-        //public IMoviesRepository MoviesRepository { get { return _moviesRepository; } }
-        //public IUsersRepository UsersRepository { get { return _usersRepository; } }
+        public IDirectorsRepository DirectorsRepository { get { return _directorsRepository; } }
+        public IMoviesRepository MoviesRepository { get { return _moviesRepository; } }
+        public IUsersRepository UsersRepository { get { return _usersRepository; } }
         public IGenresRepository GenresRepository { get { return _genresRepository; } }
 
         public async Task Save()
