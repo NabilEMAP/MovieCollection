54bd635d56d1b91cf86c34a307799a01b81d6a9d
NabilEMAP <Nabil_EM@outlook.com>
2023-01-07 14:52:57 +0100

    MovieController Constructor

--- MovieCollection.UI/Models/MovieViewModel.cs
+++ MovieCollection.UI/Models/MovieViewModel.cs
@@ -1,9 +1,11 @@
-﻿using MovieCollection.DAL.Models;
-
-namespace MovieCollection.UI.Models
+﻿namespace MovieCollection.UI.Models
 {
     public class MovieViewModel
     {
+        public MovieViewModel()
+        {
+            Country = new CountryViewModel();
+        }
         public int Id { get; set; }
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
