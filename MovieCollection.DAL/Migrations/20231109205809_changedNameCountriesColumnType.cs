49b1a91b5d270b19b9a5df423f25042778154adc
NabilEMAP <Nabil_EM@outlook.com>
2024-09-27 21:23:47 +0200

    Current migrations

---
+++ MovieCollection.DAL/Migrations/20231109205809_changedNameCountriesColumnType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieCollection.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedNameCountriesColumnType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "Country",
+                table: "tblCountries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "Country",
+                table: "tblCountries",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
